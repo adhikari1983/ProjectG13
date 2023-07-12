170639c4e712fe8d1795762f74e51e30e62271c3
SPNYC <pisarevasvetlana84@gmail.com>
2023-07-12 13:03:11 -0400

    RegressionRunner Class  in runners package and Constants Class in Utils package are added

---
+++ src/test/java/runners/RegressionRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="scr/test/resources/features/", //this is the path to all features files
+        glue="Steps",                           //keyword to the package with step definitions
+        dryRun=false,                          //dryRun=true -> to provide the missing step definition
+        tags="@regression",                   //test tag
+        monochrome=true,                     //to provide simple out put on the console
+        plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json"}
+                                            //to print all the steps in the console and generate reports
+)
+
+public class RegressionRunner {
+}
