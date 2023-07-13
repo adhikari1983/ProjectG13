7b7e2363d651e29ebbca0a787e325fbe5bcab1ac
sarahemkk <semakkie36@gmail.com>
2023-07-12 22:42:55 -0400

    common methods added

---
+++ src/test/java/Utilis/CommonMethods.java
@@ -0,0 +1,118 @@
+package Utilis;
+
+import org.apache.commons.collections4.Put;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+
+public class CommonMethods {
+    public static WebDriver driver;
+
+    public static void openBrowserAndNavigateToURL() {
+
+        //the path is coming from the constants
+        ConfigReader.readProperties(Constants.CONFIG_READER_PATH);
+
+
+
+        //read the value of the property "url" from the config file
+        switch (ConfigReader.getPropertyValue("browser")) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+        }
+
+
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        // this method is going to initialize all the objects
+        initializePageObjects();
+    }
+
+    //close the browser
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    public  static void sendText(String text, WebElement element){
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public static WebDriverWait getWait() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        return wait;
+    }
+
+    public static void waitForClickability(WebElement element) {
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void click(WebElement element){
+        waitForClickability(element);
+        element.click();
+    }
+
+    public static void selectFromDropdown(WebElement dropDown, String visibleText) {
+        Select sel = new Select(dropDown);
+        sel.selectByVisibleText(visibleText);
+    }
+
+    public static void selectFromDropdown(String value, WebElement dropDown) {
+        Select sel = new Select(dropDown);
+        sel.selectByValue(value);
+    }
+
+    public static void selectFromDropdown(WebElement dropDown, int index) {
+        Select sel = new Select(dropDown);
+        sel.selectByIndex(index);
+    }
+    // to take screen-shots and store
+    public static byte[] takeScreenshot(String fileName){
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        //we write this line because cucumber accepts array of byte for screenshot
+        byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
+        //to store the files
+        File screenShot = ts.getScreenshotAs(OutputType.FILE);
+        //in case if it doesn't find file name or path it will throw an exception
+
+        try{
+            FileUtils.copyFile(screenShot,
+                    new File(Constants.SCREENSHOT_FILEPATH + fileName+" "
+                            +getTimeStamp("yyyy-MM-dd-HH-mm-ss")+".png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return picBytes;
+    }
+
+    public static String getTimeStamp(String pattern){
+        //it returns the current date and time in java
+        Date date = new Date();
+        //this function sdf used to format the date as per the pattern we are passing
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        //this line is going to return the formatted date
+        return sdf.format(date);
+    }
+
+}
+
+
+
